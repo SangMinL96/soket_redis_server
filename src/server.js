a033decb25fa61010ac3e38c7986fc370012c6d7
SangMin96 <hwon3794@gmail.com>
2021-02-02 16:14:30 +0900

    .

--- src/server.js
+++ src/server.js
@@ -1,34 +1,41 @@
-import express from 'express'
+import express from "express";
 const app = express();
 const port = 3814;
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const redisAdapter = require('socket.io-redis');
-io.adapter(redisAdapter({ host: '172.16.2.118', port: 6379 }));
-app.get('/', (req, res, next) => {
-    res.send('hello world!');
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const redisAdapter = require("socket.io-redis");
+const redis = require("redis");
+const redisClient = redis.createClient({ host: "193.123.224.133", port: 6379 });
+io.adapter(redisAdapter({ host: "193.123.224.133", port: 6379 }));
+app.get("/", (req, res, next) => {
+  res.send("hello world!");
 });
 
-io.on('connection',(socket) => {
-    // joinRoom을 클라이언트가 emit 했을 시
-    //유저가 방입장시 방id에 따라 우선적으로 조인 시킴
-    socket.on('joinRoom',async function(msg) {  
-        console.log(msg)
-        let roomId = msg;
-        socket.join(roomId);  
+io.on("connection", (socket) => {
+  // joinRoom을 클라이언트가 emit 했을 시
+  //유저가 방입장시 방id에 따라 우선적으로 조인 시킴
+  socket.on("joinRoom", (roomId) => {
+    redisClient.lrange(roomId, 0, -1, (err, data) => {
+      const datas= []
+      data?.forEach(item=>datas.unshift(JSON.parse(item)))
+      socket.emit("joinData", datas);
     });
-
-    //채팅 메시지 대기후 받으면 방아이디에 대한 emit을 보냄
-    socket.on('chatting',async (msg) => {
-        console.log(msg)
-        
-        io.to(msg.roomId).emit("room1",msg.msg)
-
-      });
+    socket.join(roomId);
   });
 
+  //채팅 메시지 대기후 받으면 방아이디에 대한 emit을 보냄
+  socket.on("chatting", (msg) => {
+    redisClient.lpush(
+      msg.roomId,
+      `{"avatar":"${msg.avatar}","name":"${msg.name}","msg":"${msg.msg}","id":"${msg.id}"}`
+    );
+  console.log(msg)
+    io.to(msg.roomId).emit("message", msg);
+  });
+});
+
 http.listen(port, () => {
-    console.log(`localhost:${port}`);
+  console.log(`localhost:${port}`);
 });
 
 // $env:NODE_ENV="dev"; yarn dev
